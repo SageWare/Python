998af8dda532e6290759812807b71ced596a2ab7
Sage Fremont <36422856+SageWare@users.noreply.github.com>
2020-01-08 06:23:27 -0500

    Create blastoff.py

---
+++ blastoff/blastoff.py
@@ -0,0 +1,23 @@
+# Get user input as a string
+userInput = (int)(input("Please enter an integer: "))
+
+
+def countup(n):
+    if n >= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countup(n+1)
+
+
+def countdown(o):
+    if o <= 0:
+        print('Blastoff!')
+    else:
+        print(o)
+        countdown(o-1)
+
+if userInput >= 0:
+    countdown(userInput)
+if userInput <= 0:
+    countup(userInput)
